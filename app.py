fa5054c5c3ee68238eadda77fe57664d358061d2
Rob Cowen <robcowen@gmail.com>
2019-11-02 22:27:00 +0000

    Voter index now returned from database

--- app.py
+++ app.py
@@ -1,7 +1,17 @@
 from flask import Flask, render_template, request, redirect
 import requests
 import json
+import os, csv
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker
+
 app = Flask(__name__)
+
+engine = create_engine(os.getenv("JAWSDB_URL")) # database engine object from SQLAlchemy that manages connections to the database
+                                                # DATABASE_URL is an environment variable that indicates where the database lives
+db = scoped_session(sessionmaker(bind=engine))    # create a 'scoped session' that ensures different users' interactions with the
+                                                # database are kept separate
 
 
 @app.route('/')
@@ -33,8 +43,12 @@ def postcode_search():
 
 @app.route('/<string:constituency>')
 def results(constituency):
-    
-    return constituency
+
+    results = db.execute("SELECT * FROM power_index WHERE constituency = :constituency", {"constituency": constituency}).fetchone()
+
+    voter_index = results.voter_index
+
+    return render_template("results.html", constituency = constituency, voter_index = voter_index)
 
 
 @app.route('/fancyvisuals', methods=['POST'])
