51b35ecd216f77cdb31e1fc00243adc88411aebc
Rob Cowen <robcowen@gmail.com>
2019-11-02 17:21:36 +0000

    Postcode search works

--- app.py
+++ app.py
@@ -1,10 +1,35 @@
-from flask import Flask
+from flask import Flask, render_template, request
+import requests
+import json
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
-    return 'Voter Power Index'
+def index():
+    return render_template("index.html")
+
+@app.route('/postcode_search', methods=['POST'])
+def postcode_search():
+
+    postcode = request.form['postcode']
+
+    # Query API
+
+    r = requests.get(
+      'https://api.postcodes.io/postcodes/'+postcode)
+
+    if r:
+        print("Success")
+    else:
+        print("Error: API unavailable")
+
+    r = r.json()
+
+    result = r['result']
+
+    print(result)
+
+    return result.get("parliamentary_constituency")
 
 if __name__ == '__main__':
     app.run()
